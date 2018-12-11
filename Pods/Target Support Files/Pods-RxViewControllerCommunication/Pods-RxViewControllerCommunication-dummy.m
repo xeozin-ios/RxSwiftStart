63f70636bbc4e220faa8694cf0e476b0d3b38d00
SJ.H <comcxx11@gmail.com>
2018-12-11 14:49:59 +0900

    [ SJ.H ] : first commit

---
+++ Pods/Target Support Files/Pods-RxViewControllerCommunication/Pods-RxViewControllerCommunication-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxViewControllerCommunication : NSObject
+@end
+@implementation PodsDummy_Pods_RxViewControllerCommunication
+@end
---
+++ Pods/Target Support Files/Pods-RxViewControllerCommunication/Pods-RxViewControllerCommunication-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_RxViewControllerCommunicationVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_RxViewControllerCommunicationVersionString[];
+
